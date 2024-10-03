436d8b06be81c4e4d351303b55a927f89ecf34f8
calo100 <calocanadas@gmail.com>
2024-10-03 14:27:34 -0600

    Add files via upload

---
+++ Dinorah/contador/script.js
@@ -0,0 +1,15 @@
+function inicio() {
+    let segundos = +document.querySelector('#segundos').value; 
+    setTimeout(finalizar, segundos * 1000);
+}  
+function finalizar() {
+    let salida = document.querySelector('#salida');
+    salida.textContent = 'ON';
+    salida.style = 'color: green; font-size: 25px';
+
+    let audio = document.querySelector('#alerta');
+    audio.play();
+}
+function resetear() {
+    location.reload();
+}   
